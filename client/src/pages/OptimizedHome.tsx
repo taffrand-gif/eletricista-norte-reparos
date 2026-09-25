d9f8df27c71e7ba6efe1556d38be7ce9fff070f6
Hermes (default) <hermes@norte-reparos.local>
2026-09-25 23:29:04 +0100

    fix: remove legacy service price calculator from homepage

--- client/src/pages/OptimizedHome.tsx
+++ client/src/pages/OptimizedHome.tsx
@@ -7,7 +7,6 @@ import Diagnostico from '@/components/Diagnostico';
 import OrcamentoGratuitoBadge from '@/components/OrcamentoGratuitoBadge';
 import OptimizedServices from '@/components/OptimizedServices';
 import StatsCounters from '@/components/StatsCounters';
-import CalculadorPreco from '@/components/CalculadorPreco';
 import ZonaIntervencao from '@/components/ZonaIntervencao';
 import Equipa from '@/components/Equipa';
 import Garantias from '@/components/Garantias';
